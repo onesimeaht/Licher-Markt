58fc42e46be3c3342d85193cf9b6db4b2d6394cc
@onesimeahtonesimeaht <onesime.ahoton@epitech.eu>
2025-06-19 11:49:07 +0200

    Adding my app routes and linkage between all my page

--- Licher_Markt/routes/web.php
+++ Licher_Markt/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AutoriteController;
+use App\Http\Controllers\RealisationController;
+use App\Models\Realisation;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RealisationController::class, 'home'])->name('realisation.home');
+Route::get('/autorites', [AutoriteController::class, 'index'])->name('autorite.index');
+Route::get('/autorite/{id}', [AutoriteController::class, 'show'])->name('autorite.show');
+Route::get('/realisations', [RealisationController::class, 'index'])->name('realisation.index');
+Route::get('/find', [RealisationController::class, 'find'])->name('realisation.find');
+Route::post('/result', [RealisationController::class, 'filtre'])->name('realisation.filtre');
